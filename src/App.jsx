8ff7755cd81e1fe0176caebe4b6394c026dd80c1
Yeshwanth Krishna <yeshwanthkrishna37@gmail.com>
2026-06-17 21:58:19 +0530

    fix: fixed lint, glob

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 import { compressImageToTarget } from "./utils/compressor.js";
 import { PORTAL_PRESETS } from "./utils/portalPresets";
